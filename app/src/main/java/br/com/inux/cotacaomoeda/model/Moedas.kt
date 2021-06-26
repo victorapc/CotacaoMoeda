a4af0c4a98ab308ababc721138ecd8f8b6cf5ed8
victor <victor.alexandre@atlantasistemas.com>
2021-06-26 12:43:42 -0300

    Implementação do recurso da API comunicando para pegar os dados da cotação das moedas.

--- app/src/main/java/br/com/inux/cotacaomoeda/model/Moedas.kt
+++ app/src/main/java/br/com/inux/cotacaomoeda/model/Moedas.kt
@@ -2,7 +2,8 @@ package br.com.inux.cotacaomoeda.model
 
 data class Moedas (
     val id: Int,
-    val moeda: String
+    val moeda: String,
+    val tipoMoeda: String
 ) {
     override fun toString(): String {
         return moeda
--- app/src/main/java/br/com/inux/cotacaomoeda/ui/MainActivity.kt
+++ app/src/main/java/br/com/inux/cotacaomoeda/ui/MainActivity.kt
@@ -3,16 +3,34 @@ package br.com.inux.cotacaomoeda.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Adapter
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import br.com.inux.cotacaomoeda.R
 import br.com.inux.cotacaomoeda.databinding.ActivityMainBinding
 import br.com.inux.cotacaomoeda.model.Moedas
+import br.com.inux.cotacaomoeda.model.ValorMoedasModel
+import br.com.inux.cotacaomoeda.repository.Repository
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.EUR_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.GBP_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.USD_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.USD_BRLT
+import com.inux.retrofitapp.viewmodel.MainViewModel
+import com.inux.retrofitapp.viewmodel.MainViewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
+    val lista: ArrayList<Moedas> by lazy {
+        ArrayList()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +38,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+
         binding.moedasAu.onItemClickListener =
             AdapterView.OnItemClickListener { parent, view, position, id ->
-                Toast.makeText(this@MainActivity, "Posição: ${position}", Toast.LENGTH_LONG).show()
+                carregarDadosApi(lista[position])
             }
     }
 
@@ -32,15 +54,55 @@ class MainActivity : AppCompatActivity() {
         carregarSpinner()
     }
 
+    private fun carregarDadosApi(moeda: Moedas){
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                viewModel.getListMoeda(moeda.tipoMoeda)
+
+                viewModel.myListResponse.observe(this@MainActivity, Observer { response ->
+                    if(response.isSuccessful){
+                        response.body()?.let { moedas ->
+                            val moeda = moedas[0]
+
+                            Toast.makeText(this@MainActivity, "Posição: ${moeda.name}", Toast.LENGTH_LONG).show()
+                        }
+                    }else{
+                        when(response.code()){
+                            404 -> Toast.makeText(this@MainActivity, getString(R.string.erro_404), Toast.LENGTH_LONG).show()
+                            else -> {
+                                Toast.makeText(this@MainActivity, getString(R.string.erro_geral), Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+                })
+            }catch (e: Exception){
+                Toast.makeText(this@MainActivity, "Erro ao comunicar com o servidor", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
     private fun carregarSpinner(){
-        val lista = ArrayList<Moedas>()
-        lista.add(Moedas(1, "Dólar Americano / Real Brasileiro"))
-        lista.add(Moedas(2, "Dólar Americano / Real Brasileiro Turismo"))
-        lista.add(Moedas(3, "Euro / Real Brasileiro"))
-        lista.add(Moedas(4, "Libra Esterlina / Real Brasileiro"))
+        lista.add(Moedas(1, "Dólar Americano / Real Brasileiro", USD_BRL))
+        lista.add(Moedas(2, "Dólar Americano / Real Brasileiro Turismo", USD_BRLT))
+        lista.add(Moedas(3, "Euro / Real Brasileiro", EUR_BRL))
+        lista.add(Moedas(4, "Libra Esterlina / Real Brasileiro", GBP_BRL))
 
         val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, lista)
 
         binding.moedasAu.setAdapter(arrayAdapter)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_update  ->
+                Toast.makeText(this, "Atualizar", Toast.LENGTH_LONG).show()
+            else -> false
+        }
+        return super.onOptionsItemSelected(item)
     }
 }

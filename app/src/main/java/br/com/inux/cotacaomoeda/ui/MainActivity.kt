485269ebb766c31184726a8bcfde30071d4dbc46
victor <victor.alexandre@atlantasistemas.com>
2021-06-26 18:31:42 -0300

    Adicionado as moedas da api e inicio do calculo.

--- app/src/main/java/br/com/inux/cotacaomoeda/ui/MainActivity.kt
+++ app/src/main/java/br/com/inux/cotacaomoeda/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package br.com.inux.cotacaomoeda.ui
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
@@ -13,12 +16,46 @@ import androidx.lifecycle.ViewModelProvider
 import br.com.inux.cotacaomoeda.R
 import br.com.inux.cotacaomoeda.databinding.ActivityMainBinding
 import br.com.inux.cotacaomoeda.model.Moedas
-import br.com.inux.cotacaomoeda.model.ValorMoedasModel
 import br.com.inux.cotacaomoeda.repository.Repository
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.AED_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.ARS_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.AUD_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.BOB_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.BTC_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.CAD_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.CHF_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.CLP_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.CNY_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.COP_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.DKK_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.DOGE_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.ETH_BRL
 import br.com.inux.cotacaomoeda.utils.Constants.Companion.EUR_BRL
 import br.com.inux.cotacaomoeda.utils.Constants.Companion.GBP_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.HKD_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.ILS_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.INR_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.JPY_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.LTC_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.MXN_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.NOK_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.NZD_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.PEN_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.PLN_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.PYG_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.RUB_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.SAR_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.SEK_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.SGD_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.THB_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.TRY_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.TWD_BRL
 import br.com.inux.cotacaomoeda.utils.Constants.Companion.USD_BRL
 import br.com.inux.cotacaomoeda.utils.Constants.Companion.USD_BRLT
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.UYU_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.XRP_BRL
+import br.com.inux.cotacaomoeda.utils.Constants.Companion.ZAR_BRL
+import br.com.inux.cotacaomoeda.utils.MetodosGlobais
 import com.inux.retrofitapp.viewmodel.MainViewModel
 import com.inux.retrofitapp.viewmodel.MainViewModelFactory
 import kotlinx.coroutines.CoroutineScope
@@ -26,8 +63,13 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    // Objetos.
+    private lateinit var globais: MetodosGlobais
+    private lateinit var moeda: Moedas
+
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
+    private var valorMoeda: Double = 0.0
     val lista: ArrayList<Moedas> by lazy {
         ArrayList()
     }
@@ -38,13 +80,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
+        // Instanciando objetos
+        globais = MetodosGlobais(this@MainActivity)
+
         val repository = Repository()
         val viewModelFactory = MainViewModelFactory(repository)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
 
         binding.moedasAu.onItemClickListener =
             AdapterView.OnItemClickListener { parent, view, position, id ->
-                carregarDadosApi(lista[position])
+                moeda = lista[position]
+
+                carregarDadosMoeda()
             }
     }
 
@@ -54,9 +101,24 @@ class MainActivity : AppCompatActivity() {
         carregarSpinner()
     }
 
+    private fun carregarDadosMoeda(){
+        val connectivity = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if(globais.existeConexao(connectivity)){
+            carregarDadosApi(moeda)
+        } else {
+            Toast.makeText(
+                this@MainActivity,
+                "É necessário uma conexão para buscar a cotação das moedas.",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
     private fun carregarDadosApi(moeda: Moedas){
         CoroutineScope(Dispatchers.Main).launch {
             try {
+                binding.recylerProgressBar.visibility = View.VISIBLE
+
                 viewModel.getListMoeda(moeda.tipoMoeda)
 
                 viewModel.myListResponse.observe(this@MainActivity, Observer { response ->
@@ -64,9 +126,19 @@ class MainActivity : AppCompatActivity() {
                         response.body()?.let { moedas ->
                             val moeda = moedas[0]
 
-                            Toast.makeText(this@MainActivity, "Posição: ${moeda.name}", Toast.LENGTH_LONG).show()
+                            valorMoeda = moeda.bid
+                            binding.compraVlrTxt.text = "R$ ${globais.formataValor(valorMoeda)}"
+                            binding.vendaVlrTxt.text = "R$ ${globais.formataValor(moeda.ask)}"
+                            binding.altaVlrTxt.text = "R$ ${globais.formataValor(moeda.high)}"
+                            binding.baixaVlrTxt.text = "R$ ${globais.formataValor(moeda.low)}"
+
+                            binding.valorEdt.setText("1")
+
+                            binding.recylerProgressBar.visibility = View.GONE
                         }
                     }else{
+                        binding.recylerProgressBar.visibility = View.GONE
+
                         when(response.code()){
                             404 -> Toast.makeText(this@MainActivity, getString(R.string.erro_404), Toast.LENGTH_LONG).show()
                             else -> {
@@ -76,16 +148,60 @@ class MainActivity : AppCompatActivity() {
                     }
                 })
             }catch (e: Exception){
-                Toast.makeText(this@MainActivity, "Erro ao comunicar com o servidor", Toast.LENGTH_LONG).show()
+                binding.recylerProgressBar.visibility = View.GONE
+
+                Toast.makeText(this@MainActivity, getString(R.string.erro_geral), Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    private fun calcular(){
+        var valorDigitado = 0.0
+
+        if(binding.valorEdt.text?.isEmpty() == true){
+            valorDigitado = 1.0
         }
     }
 
     private fun carregarSpinner(){
         lista.add(Moedas(1, "Dólar Americano / Real Brasileiro", USD_BRL))
         lista.add(Moedas(2, "Dólar Americano / Real Brasileiro Turismo", USD_BRLT))
-        lista.add(Moedas(3, "Euro / Real Brasileiro", EUR_BRL))
-        lista.add(Moedas(4, "Libra Esterlina / Real Brasileiro", GBP_BRL))
+        lista.add(Moedas(3, "Dólar Canadense / Real Brasileiro", CAD_BRL))
+        lista.add(Moedas(4, "Euro / Real Brasileiro", EUR_BRL))
+        lista.add(Moedas(5, "Libra Esterlina / Real Brasileiro", GBP_BRL))
+        /*lista.add(Moedas(6, "Bitcoin / Real Brasileiro", BTC_BRL))
+        lista.add(Moedas(7, "Dogecoin / Real Brasileiro", DOGE_BRL))
+        lista.add(Moedas(8, "Ethereum / Real Brasileiro", ETH_BRL))
+        lista.add(Moedas(9, "Litecoin/Real Brasileiro", LTC_BRL))*/
+        lista.add(Moedas(10, "Peso Argentino / Real Brasileiro", ARS_BRL))
+        lista.add(Moedas(11, "Peso Chileno / Real Brasileiro", CLP_BRL))
+        lista.add(Moedas(12, "Peso Uruguaio / Real Brasileiro", UYU_BRL))
+        lista.add(Moedas(13, "Guarani Paraguaio / Real Brasileiro", PYG_BRL))
+        lista.add(Moedas(14, "Peso Colombiano / Real Brasileiro", COP_BRL))
+        lista.add(Moedas(15, "Boliviano / Real Brasileiro", BOB_BRL))
+        lista.add(Moedas(16, "Dólar Australiano / Real Brasileiro", AUD_BRL))
+        lista.add(Moedas(17, "Peso Mexicano / Real Brasileiro", MXN_BRL))
+        lista.add(Moedas(18, "Sol do Peru / Real Brasileiro", PEN_BRL))
+        lista.add(Moedas(19, "Iene Japonês / Real Brasileiro", JPY_BRL))
+        lista.add(Moedas(20, "Franco Suíço / Real Brasileiro", CHF_BRL))
+        lista.add(Moedas(21, "Yuan Chinês / Real Brasileiro", CNY_BRL))
+        lista.add(Moedas(22, "Novo Shekel Israelense / Real Brasileiro", ILS_BRL))
+        lista.add(Moedas(23, "XRP / Real Brasileiro", XRP_BRL))
+        lista.add(Moedas(24, "Dólar de Cingapura / Real Brasileiro", SGD_BRL))
+        lista.add(Moedas(25, "Dirham dos Emirados / Real Brasileiro", AED_BRL))
+        lista.add(Moedas(26, "Coroa Dinamarquesa / Real Brasileiro", DKK_BRL))
+        lista.add(Moedas(27, "Dólar de Hong Kong / Real Brasileiro", HKD_BRL))
+        lista.add(Moedas(28, "Coroa Norueguesa / Real Brasileiro", NOK_BRL))
+        lista.add(Moedas(29, "Dólar Neozelandês / Real Brasileiro", NZD_BRL))
+        lista.add(Moedas(30, "Zlóti Polonês / Real Brasileiro", PLN_BRL))
+        lista.add(Moedas(31, "Riyal Saudita / Real Brasileiro", SAR_BRL))
+        lista.add(Moedas(32, "Coroa Sueca / Real Brasileiro", SEK_BRL))
+        lista.add(Moedas(33, "Baht Tailandês / Real Brasileiro", THB_BRL))
+        lista.add(Moedas(34, "Nova Lira Turca / Real Brasileiro", TRY_BRL))
+        lista.add(Moedas(35, "Dólar Taiuanês / Real Brasileiro", TWD_BRL))
+        lista.add(Moedas(36, "Rand Sul-Africano / Real Brasileiro", ZAR_BRL))
+        lista.add(Moedas(37, "Rublo Russo / Real Brasileiro", RUB_BRL))
+        lista.add(Moedas(38, "Rúpia Indiana / Real Brasileiro", INR_BRL))
 
         val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, lista)
 
@@ -100,7 +216,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.menu_update  ->
-                Toast.makeText(this, "Atualizar", Toast.LENGTH_LONG).show()
+                carregarDadosMoeda()
             else -> false
         }
         return super.onOptionsItemSelected(item)
--- app/src/main/java/br/com/inux/cotacaomoeda/utils/Constants.kt
+++ app/src/main/java/br/com/inux/cotacaomoeda/utils/Constants.kt
@@ -4,9 +4,43 @@ class Constants {
     companion object{
         const val BASE_URL = "https://economia.awesomeapi.com.br/"
 
-        const val USD_BRL = "USD-BRL"
-        const val USD_BRLT = "USD-BRLT"
-        const val EUR_BRL = "EUR-BRL"
-        const val GBP_BRL = "GBP-BRL"
+        const val USD_BRL = "USD-BRL"   // Dólar Americano/Real Brasileiro
+        const val USD_BRLT = "USD-BRLT" // Dólar Americano/Real Brasileiro Turismo
+        const val CAD_BRL = "CAD-BRL"   // Dólar Canadense/Real Brasileiro
+        const val EUR_BRL = "EUR-BRL"   // Euro/Real Brasileiro
+        const val GBP_BRL = "GBP-BRL"   // Libra Esterlina/Real Brasileiro
+        const val BTC_BRL = "BTC-BRL"   // Bitcoin/Real Brasileiro
+        const val DOGE_BRL = "DOGE-BRL" // Dogecoin/Real Brasileiro
+        const val ETH_BRL = "ETH-BRL"   // Ethereum/Real Brasileiro
+        const val LTC_BRL = "LTC-BRL"   // Litecoin/Real Brasileiro
+        const val ARS_BRL = "ARS-BRL"   // Peso Argentino/Real Brasileiro
+        const val CLP_BRL = "CLP-BRL"   // Peso Chileno/Real Brasileiro
+        const val UYU_BRL = "UYU-BRL"   // Peso Uruguaio/Real Brasileiro
+        const val PYG_BRL = "PYG-BRL"   // Guarani Paraguaio/Real Brasileiro
+        const val COP_BRL = "COP-BRL"   // Peso Colombiano/Real Brasileiro
+        const val BOB_BRL = "BOB-BRL"   // Boliviano/Real Brasileiro
+        const val AUD_BRL = "AUD-BRL"   // Dólar Australiano/Real Brasileiro
+        const val MXN_BRL = "MXN-BRL"   // Peso Mexicano/Real Brasileiro
+        const val PEN_BRL = "PEN-BR"    // Sol do Peru/Real Brasileiro
+        const val JPY_BRL = "JPY-BRL"   // Iene Japonês/Real Brasileiro
+        const val CHF_BRL = "CHF-BRL"   // Franco Suíço/Real Brasileiro
+        const val CNY_BRL = "CNY-BRL"   // Yuan Chinês/Real Brasileiro
+        const val ILS_BRL = "ILS-BRL"   // Novo Shekel Israelense/Real Brasileiro
+        const val XRP_BRL = "XRP-BRL"   // XRP/Real Brasileiro
+        const val SGD_BRL = "SGD-BRL"   // Dólar de Cingapura/Real Brasileiro
+        const val AED_BRL = "AED-BRL"   // Dirham dos Emirados/Real Brasileiro
+        const val DKK_BRL = "DKK-BRL"   // Coroa Dinamarquesa/Real Brasileiro
+        const val HKD_BRL = "HKD-BRL"   // Dólar de Hong Kong/Real Brasileiro
+        const val NOK_BRL = "NOK-BRL"   // Coroa Norueguesa/Real Brasileiro
+        const val NZD_BRL = "NZD-BRL"   // Dólar Neozelandês/Real Brasileiro
+        const val PLN_BRL = "PLN-BRL"   // Zlóti Polonês/Real Brasileiro
+        const val SAR_BRL = "SAR-BRL"   // Riyal Saudita/Real Brasileiro
+        const val SEK_BRL = "SEK-BRL"   // Coroa Sueca/Real Brasileiro
+        const val THB_BRL = "THB-BRL"   // Baht Tailandês/Real Brasileiro
+        const val TRY_BRL = "TRY-BRL"   // Nova Lira Turca/Real Brasileiro
+        const val TWD_BRL = "TWD-BRL"   // Dólar Taiuanês/Real Brasileiro
+        const val ZAR_BRL = "ZAR-BRL"   // Rand Sul-Africano/Real Brasileiro
+        const val RUB_BRL = "RUB-BRL"   // Rublo Russo/Real Brasileiro
+        const val INR_BRL = "INR-BRL"   // Rúpia Indiana/Real Brasileiro
     }
 }
